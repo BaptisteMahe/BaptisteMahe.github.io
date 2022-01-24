28227ec135bf4c058ad26a1b8b3a7275274b4cf9
BaptisteMahe <mahe.baptiste.19@gmail.com>
2022-01-24 23:25:43 +0100

    Add label to sliders

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,15 @@
 const flock = [];
 const FLOCK_START_SIZE = 50;
-const FLOCK_MAX_SIZE = 150;
+const FLOCK_MAX_SIZE = 100;
 
 let alignSlider, cohesionSlider, separationSlider;
 
 function setup() {
     createCanvas(800, 560);
 
-    alignSlider = createSlider(0, 5, 1, 0.1);
-    cohesionSlider = createSlider(0, 5, 1, 0.1);
-    separationSlider = createSlider(0, 5, 1, 0.1);
+    alignSlider = createSliderWithLabel("Alignment", 0, 5, 1, 0.1);
+    cohesionSlider = createSliderWithLabel("Cohesion", 0, 5, 1, 0.1);
+    separationSlider = createSliderWithLabel("Separation", 0, 5, 1, 0.1);
 
     for (let index = 0; index < FLOCK_START_SIZE; index++)
         flock.push(new Boid({ x: random(width), y: random(height) }));
@@ -29,4 +29,12 @@ function draw() {
         flock.push(new Boid({ x: mouseX, y: mouseY }));
         if (flock.length > FLOCK_MAX_SIZE) flock.shift();
     }
+}
+
+function createSliderWithLabel(label, start, stop, init, step) {
+    const labelDiv = createDiv(label);
+    labelDiv.addClass("slider")
+    const slider = createSlider(start, stop, init, step);
+    slider.parent(labelDiv);
+    return slider;
 }
